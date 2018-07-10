72e41604a9997601a942597397905dc683b18b21
melodytng <melodytang2002@yahoo.com>
2018-07-10 18:27:40 +0000

    automatically backed up by learn

---
+++ holiday_greeting.rb
@@ -0,0 +1,6 @@
+def holiday_greeting(to="Mom",from="Your Favorite Child",holiday="Mother's Day")
+  puts "Happy #{holiday}, #{to}! -From #{from}"
+end
+
+holiday_greeting
+holiday_greeting("Dad", "Philip","Father's Day")
